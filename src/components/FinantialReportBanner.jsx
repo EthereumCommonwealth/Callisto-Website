48d7f64e7f2f20d73343a5a6734fcb51bcf406da
Carlos Sampol <sampol.90@gmail.com>
2018-04-22 15:22:19 -0500

    Changing copies

--- src/components/FinantialReportBanner.jsx
+++ src/components/FinantialReportBanner.jsx
@@ -11,7 +11,7 @@ const FinantialReportBanner = () => (
         target="_blank"
         href="https://docs.google.com/spreadsheets/d/e/2PACX-1vTHbKD9_qqc2pzivkn-U6reX6pR3WF1-ryX-ZRNtCy54bg0q8JaM_VcoctRll1SgiX7t7UwN1BqOZN-/pubhtml?gid=1393329941&single=true"
       >
-        See it
+        View
       </a>
     </div>
   </div>
