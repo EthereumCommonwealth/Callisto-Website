4eb822603f8ab9dd7747c9b2fa61fe527a74ecf1
Carlos Sampol <sampol.90@gmail.com>
2018-07-13 19:22:54 -0500

    Adding Blog post list Page

---
+++ src/app/components/Blog/PostsList.jsx
@@ -0,0 +1,55 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedDate } from 'react-intl';
+import { connect } from 'react-redux';
+
+class PostsList extends PureComponent {
+  render() {
+    const { blogPosts } = this.props;
+    return (
+      <section className='PostsList'>
+        <div className='PostsList-content container'>
+          <div />
+          <div className='PostsList-list'>
+            {blogPosts.map(post => (
+              <div key={`Post-${post.id}`} className='Post'>
+                <h2 className='Post-title'>{post.title}</h2>
+                <div className='Post-date'>
+                  <FormattedDate
+                    value={new Date(post.date)}
+                    year='numeric'
+                    month='long'
+                    day='2-digit'
+                  />
+                </div>
+                <figure className='Post-figure'>
+                  <img src={post.cover} alt={post.title}/>
+                </figure>
+                <div
+                  className='Post-description'
+                  dangerouslySetInnerHTML={{ __html: post.description }}
+                />
+                <a href={post.link} className='Post-readmore'>
+                  <i className='fas fa-long-arrow-alt-right' /> read more
+                </a>
+              </div>
+            ))}
+          </div>
+          <div />
+        </div>
+      </section>
+    )
+  }
+}
+
+function mapStateTopProps(state) {
+  return {
+    blogPosts: state.blogPosts,
+  };
+}
+
+PostsList.propTypes = {
+  blogPosts: PropTypes.array,
+};
+
+export default connect(mapStateTopProps, null)(PostsList);
--- src/app/containers/Blog.jsx
+++ src/app/containers/Blog.jsx
@@ -3,14 +3,13 @@ import { hot } from 'react-hot-loader';
 import Layout from '../components/commons/Layout';
 import Header from '../components/commons/Header';
 import Footer from '../components/commons/Footer';
+import PostsList from '../components/Blog/PostsList';
 
 const Blog = ({ match }) => {
   return (
     <Layout className='Blog' match={match}>
       <Header lang={match.params && match.params.lang ? match.params.lang : 'en'} />
-      <div className='container'>
-        Blog Page
-      </div>
+      <PostsList />
       <Footer lang={match.params && match.params.lang ? match.params.lang : 'en'} />
     </Layout>
   );
