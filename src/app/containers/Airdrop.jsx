40db887e52e390f6cf78020955d49c2f30fd9d2b
Carlos Sampol <sampol.90@gmail.com>
2018-07-14 16:39:09 -0500

    adding community section to airdrop page

--- src/app/containers/Airdrop.jsx
+++ src/app/containers/Airdrop.jsx
@@ -4,6 +4,7 @@ import Layout from '../components/commons/Layout';
 import Header from '../components/commons/Header';
 import AirdropHero from '../components/Airdrop/AirdropHero';
 import AirdropDescription from '../components/Airdrop/AirdropDescription';
+import Community from '../components/Home/Community';
 import Footer from '../components/commons/Footer';
 
 const Airdrop = ({ match }) => {
@@ -12,6 +13,9 @@ const Airdrop = ({ match }) => {
       <Header lang={match.params && match.params.lang ? match.params.lang : 'en'} />
       <AirdropHero />
       <AirdropDescription />
+      <Community
+        lang={match.params && match.params.lang ? match.params.lang : 'en'}
+      />
       <Footer lang={match.params && match.params.lang ? match.params.lang : 'en'} />
     </Layout>
   );
