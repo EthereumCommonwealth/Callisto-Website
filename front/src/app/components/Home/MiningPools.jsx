7bb28e0f59ec075c074895ff511a5762bdf58be2
Carlos Sampol <sampol.90@gmail.com>
2018-08-11 12:42:11 -0500

    Finishing api integration

--- front/src/app/components/Home/MiningPools.jsx
+++ front/src/app/components/Home/MiningPools.jsx
@@ -1,163 +1,55 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Element } from 'react-scroll';
 import { injectIntl } from 'react-intl';
 import SectionHeading from '../commons/SectionHeading';
 
-const MiningPools = ({ intl }) => (
+const MiningPools = ({ intl, miningPools, blockExplorers }) => (
   <Element className='MiningPools container' name='pool' id='pool'>
     <div className='MiningPools-content'>
       <SectionHeading
         title={intl.formatMessage({ id: 'CallistoMiningPool' })}
       />
       <div className='MiningPools-list'>
-        <a
-          href='https://clopool.pro/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          clopool.pro
-        </a>
-        <a
-          href='https://clo.epool.io/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          epool.io
-        </a>
-        <a
-          href='https://coinfoundry.org/pool/clo'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          coinfoundry.org
-        </a>
-        <a
-          href='https://clo.2miners.com/en'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          2miners.com
-        </a>
-        <a
-          href='http://callistopool.org/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          callistopool.org
-        </a>
-        <a
-          href='http://www.callisto-pool.com/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          callisto-pool.com
-        </a>
-        <a
-          href='https://clo.solopool.org/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          solopool.org
-        </a>
-        <a
-          href='http://clo.mole-pool.net:89/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          mole-pool.net
-        </a>
-        <a
-          href='http://clona.ru/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          clona.ru
-        </a>
-        <a
-          href='http://clo.minerpool.net/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          minerpool.net
-        </a>
-        <a
-          href='https://clopool.net/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          clopool.net
-        </a>
-        <a
-          href='https://comining.io/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          comining.io
-        </a>
-        <a
-          href='http://clo.pool.sexy/#/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          clo.pool.sexy
-        </a>
-        <a
-          href='http://pool.baikalmine.com/en/clo/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          pool.baikalmine.com
-        </a>
-        <a
-          href='https://pplns.io/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          pplns.io
-        </a>
-        <a
-          href='https://www.vvpool.com/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          vvpool.com
-        </a>
-        <a
-          href='https://clo.topmining.co.kr/ '
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          clo.topmining.co.kr
-        </a>
+        {miningPools.length > 0 ?
+          miningPools.map(pool => (
+            <a
+              href={pool.url}
+              key={pool.name}
+              className='btn btn-transparent-green'
+              target='_blank'
+            >
+              {pool.name}
+            </a>
+          )) : null
+        }
       </div>
       <SectionHeading
         title={intl.formatMessage({ id: 'BlockExplorers' })}
       />
       <div className='MiningPools-explorers'>
-        <a
-          href='https://explorer.callisto.network/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          explorer.callisto.network
-        </a>
-        <a
-          href='https://callistoexplorer.com/home'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          callistoexplorer.com
-        </a>
-        <a
-          href='https://cloexplorer.org/'
-          className='btn btn-transparent-green'
-          target='_blank'
-        >
-          cloexplorer.org
-        </a>
+        {blockExplorers.length > 0 ?
+          blockExplorers.map(explorer => (
+            <a
+              key={explorer.name}
+              href={explorer.url}
+              className='btn btn-transparent-green'
+              target='_blank'
+            >
+              {explorer.name}
+            </a>
+          )) : null
+        }
       </div>
     </div>
   </Element>
 );
 
-export default injectIntl(MiningPools);
+function mapStateTopProps(state) {
+  return {
+    miningPools: state.miningPools,
+    blockExplorers: state.blockExplorers,
+  };
+}
+
+export default injectIntl(connect(mapStateTopProps)(MiningPools));
--- front/src/app/components/Home/WalletsContent.jsx
+++ front/src/app/components/Home/WalletsContent.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Element } from 'react-scroll';
 import { injectIntl } from 'react-intl';
 import SectionHeading from '../commons/SectionHeading';
 import SingleWallet from '../commons/SingleWallet';
 
-const WalletsContent = ({ intl }) => (
+const WalletsContent = ({ intl, wallets }) => (
   <Element className='WalletsContent' name='wallets'>
     <div className='WalletsContent-container container'>
       <SectionHeading
@@ -12,65 +13,23 @@ const WalletsContent = ({ intl }) => (
         description={intl.formatMessage({ id: 'WalletsDescription' })}
       />
       <div className='WalletsContent-list'>
-        <SingleWallet
-          icon='/image/wallets/windows.svg'
-          title='Windows'
-          options={[
-            { name: 'Geth', url: '/' },
-            { name: 'Parity', url: '/' },
-            { name: 'Guarda', url: 'https://guarda.co/desktop/' },
-            { name: 'MyCrypto', url: 'https://mycrypto.com/' },
-          ]}
-        />
-        <SingleWallet
-          icon='/image/wallets/apple.svg'
-          title='OSX'
-          options={[
-            { name: 'Geth', url: '/' },
-            { name: 'Guarda', url: 'https://guarda.co/desktop/' },
-            { name: 'MyCrypto', url: 'https://mycrypto.com/' },
-          ]}
-        />
-        <SingleWallet
-          icon='/image/wallets/linux.svg'
-          title='Linux'
-          options={[
-            { name: 'Geth', url: '/' },
-            { name: 'Parity', url: '/' },
-            { name: 'Guarda', url: 'https://guarda.co/desktop/' },
-            { name: 'MyCrypto', url: 'https://mycrypto.com/' },
-          ]}
-        />
-        <SingleWallet
-          icon='/image/wallets/playstore.svg'
-          title='Android'
-          options={[
-            { name: 'Coinomi', url: 'https://coinomi.com/' },
-            { name: 'Guarda', url: 'https://guarda.co/' },
-            { name: 'Trust Wallet', url: 'https://trustwalletapp.com/' },
-          ]}
-        />
-        <SingleWallet
-          icon='/image/wallets/applestore.svg'
-          title='iOS'
-          options={[
-            { name: 'Coinomi', url: 'https://coinomi.com/' },
-            { name: 'Guarda', url: 'https://guarda.co/' },
-            { name: 'Trust Wallet', url: 'https://itunes.apple.com/us/app/trust-ethereum-wallet/id1288339409?mt=8' }
-          ]}
-        />
-        <SingleWallet
-          icon='/image/wallets/web.svg'
-          title='Web'
-          options={[
-            { name: 'Classic Ether Wallet', url: 'https://ethereumproject.github.io/etherwallet/' },
-            { name: 'Guarda', url: 'https://guarda.co/' },
-            { name: 'MyCrypto', url: 'https://mycrypto.com/' },
-          ]}
-        />
+        {wallets.length > 0 ? wallets.map(wallet => (
+          <SingleWallet
+            key={wallet.title}
+            icon={wallet.icon}
+            title={wallet.title}
+            options={wallet.options}
+          />
+        )) : null}
       </div>
     </div>
   </Element>
 );
 
-export default injectIntl(WalletsContent);
+function mapStateTopProps(state) {
+  return {
+    wallets: state.wallets,
+  };
+}
+
+export default injectIntl(connect(mapStateTopProps)(WalletsContent));
