6958cb7f415a8681697ea5282d6935e41fe81dd9
Diego Forero <gollum23@gmail.com>
2018-08-11 21:17:24 -0500

    :wrench: change script name

--- back/docker/prod/run-production.sh
+++ back/docker/prod/run-production.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate
 
-gunicorn -w 2 callisto.wsgi:application -b 0.0.0.0:8000  --log-level info --keep-alive 200 --env DJANGO_SETTINGS_MODULE=callisto.settings.production
+gunicorn -w 2 callisto.wsgi:application -b 0.0.0.0:8000  --log-level info --keep-alive 200 --env DJANGO_SETTINGS_MODULE=callisto.settings.prod
