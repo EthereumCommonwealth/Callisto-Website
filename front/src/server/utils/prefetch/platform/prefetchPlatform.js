1e3eddfb412f41957c541c9d684b2bcf2ba698f3
Carlos Sampol <sampol.90@gmail.com>
2018-09-15 16:25:14 -0500

    Fetching csrftoken on audit list

--- front/src/server/utils/prefetch/platform/prefetchPlatform.js
+++ front/src/server/utils/prefetch/platform/prefetchPlatform.js
@@ -3,15 +3,28 @@ import handlePlatformRender from '../../render/platform/handlePlatformRender';
 
 const prefetchData = async (req, res, next) => {
   try {
-    let auditsList;
+    let auditsList, audit;
     try {
       auditsList = await axios.get(`${process.env.AUDIT_URL}audit-request/list/`);
       auditsList = auditsList.data;
     } catch (e) {
       auditsList = [];
     }
+    try {
+      audit = await axios.get(`${process.env.AUDIT_URL}audit-request/login/`);
+      audit = {
+        platform: [],
+        csrf_token: audit.data.csrf_token,
+      };
+    } catch (e) {
+      audit = {
+        platform: [],
+        csrf_token: null,
+      }
+    }
     const initialState = {
       auditsList,
+      audit,
     };
     handlePlatformRender(req, res, initialState);
   } catch (err) {
