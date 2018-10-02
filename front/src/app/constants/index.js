31eb113f7b64337107b02c57351ebc0216427487
Carlos Sampol <sampol.90@gmail.com>
2018-10-02 17:34:10 -0500

    Removing langs

--- front/src/app/constants/index.js
+++ front/src/app/constants/index.js
@@ -3,28 +3,13 @@ export const BASE_BLOG_API_URL = 'https://news.callisto.network/wp-json/wp/v2/';
 export const BASE_FRESHBOOKS_URL = 'https://callistonetwork.freshdesk.com/api/v2/';
 export const CLO_ID = 2757;
 export const BTC_ID = 1;
-export const langs = '(ar|bg|cs|de|es|fr|hi|id|it|ja|ko|nl|no|pl|pt|ro|ru|tr|uk|vi|zh)';
+export const langs = '(en|de|es|fr|ru|ko|zh)';
 export const langsArray = [
   'en',
-  'ar',
-  'bg',
-  'cs',
   'de',
   'es',
   'fr',
-  'hi',
-  'id',
-  'it',
-  'ja',
-  'ko',
-  'nl',
-  'no',
-  'pl',
-  'pt',
-  'ro',
   'ru',
-  'tr',
-  'uk',
-  'vi',
-  'zh'
+  'ko',
+  'zh',
 ];
