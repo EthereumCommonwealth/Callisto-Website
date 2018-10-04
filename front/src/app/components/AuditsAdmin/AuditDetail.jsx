2138c10a35dfbe788995ef401c25ba5f8c2d6670
Carlos Sampol <sampol.90@gmail.com>
2018-10-03 22:03:36 -0500

    Adding styles for comment

--- front/src/app/components/AuditsAdmin/AuditDetail.jsx
+++ front/src/app/components/AuditsAdmin/AuditDetail.jsx
@@ -25,8 +25,9 @@ class AuditDetail extends PureComponent {
     return (
       <div className='SingleAudit-history-element is-comment'>
         <div>
-          <span className='SingleAudit-history-element-detail'>Commented by </span>
-          <span className='SingleAudit-history-element-author'>{details.statusAuthor}</span>
+          <span className='SingleAudit-history-element-count'>{index + 1}</span>
+          <span className='SingleAudit-history-element-detail'>Comment on: {details.statusName} by </span>
+          <span className='SingleAudit-history-element-author'>{details.statusAuthor ? details.statusAuthor : 'Auditor'}</span>
         </div>
         <div>
           <p className='SingleAudit-history-element-description'>
