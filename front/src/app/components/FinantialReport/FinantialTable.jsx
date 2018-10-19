f6e7a4b7d56294e861071ad9224c0d59c954dc26
Carlos Sampol <carlossampol@Eduardos-MacBook-Pro-2.local>
2018-10-19 01:12:22 -0500

    Adding new finantial report

--- front/src/app/components/FinantialReport/FinantialTable.jsx
+++ front/src/app/components/FinantialReport/FinantialTable.jsx
@@ -15,7 +15,7 @@ const FinantialTable = ({ intl }) => (
       </div>
       <iframe
         className='FinantialTable-frame'
-        src='https://docs.google.com/spreadsheets/d/e/2PACX-1vTHbKD9_qqc2pzivkn-U6reX6pR3WF1-ryX-ZRNtCy54bg0q8JaM_VcoctRll1SgiX7t7UwN1BqOZN-/pubhtml?gid=1393329941&single=true'
+        src='/report.pdf'
         frameBorder='0'
       />
     </div>
