603235b7668560269bb4d6b445277a834a784120
Carlos Sampol <sampol.90@gmail.com>
2019-03-12 01:23:10 -0500

    Changing getManifest

--- front/src/server/getManifest.js
+++ front/src/server/getManifest.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 const getManifest = () => {
   let manifest = JSON.parse(fs.readFileSync(`${__dirname}/public/manifest.json`, 'utf8'));
   manifest = Object.values(manifest);
-  return [manifest[6], manifest[7], manifest[8]];
+  return [manifest[0], manifest[1], manifest[4]];
 };
 
 export default getManifest;
