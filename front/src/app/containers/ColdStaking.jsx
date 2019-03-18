1141fb9a90562a57c02d9ba312f6dab3824031e6
Carlos Sampol <sampol.90@gmail.com>
2019-03-18 10:05:05 -0500

    Removing cs

--- front/src/app/containers/ColdStaking.jsx
+++ front/src/app/containers/ColdStaking.jsx
@@ -6,7 +6,6 @@ import BuyMineBanner from '../components/commons/BuyMineBanner';
 import StakingHero from '../components/ColdStaking/StakingHero';
 import HowToStake from '../components/ColdStaking/HowToStake';
 import StakingDescription from '../components/ColdStaking/StakingDescription';
-import StakingInvestments from '../components/ColdStaking/StakingInvestments';
 import StakingHowWorks from '../components/ColdStaking/StakingHowWorks';
 import StakingWorkflow from '../components/ColdStaking/StakingWorkflow';
 import StakingLongStory from '../components/ColdStaking/StakingLongStory';
@@ -19,7 +18,6 @@ const ColdStaking = ({ match }) => {
       <StakingHero />
       <StakingDescription />
       <HowToStake />
-      <StakingInvestments />
       <StakingHowWorks />
       <StakingWorkflow />
       <StakingLongStory />
