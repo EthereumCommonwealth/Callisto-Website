9784539fd7350e2ba73c8b9dafaeeab4323254e7
Yohan Graterol <yohangraterol92@gmail.com>
2021-01-24 22:48:04 -0500

    First prototype

---
+++ layouts/default.vue
@@ -0,0 +1,239 @@
+<template>
+  <div>
+    <section class="hero is-main-hero">
+      <div class="hero-head">
+        <b-navbar class="container">
+          <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+              <img
+                src="~/assets/callisto_logo.svg"
+                alt="Callisto Logo"
+                height="250"
+                style="max-height: 2.5rem"
+              >
+            </b-navbar-item>
+          </template>
+          <template #end>
+            <b-navbar-dropdown label="Use Callisto" class="navbar-link-color">
+              <b-navbar-item href="#">
+                Wallets
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                Exchanges
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                Explorer
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                Applications
+              </b-navbar-item>
+            </b-navbar-dropdown>
+
+            <b-navbar-dropdown label="Learn" class="navbar-link-color">
+              <b-navbar-item href="#">
+                How to buy CLO
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                How to transfer
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                How to
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                How to
+              </b-navbar-item>
+            </b-navbar-dropdown>
+
+            <b-navbar-dropdown label="Enterprise" class="navbar-link-color">
+              <b-navbar-item href="#">
+                About
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                Contact
+              </b-navbar-item>
+            </b-navbar-dropdown>
+          </template>
+        </b-navbar>
+      </div>
+
+      <div class="hero-body container">
+        <div class="columns">
+          <div class="column left is-5-desktop" style="padding-top: 2rem">
+            <h1 class="title is-size-2-mobile is-1 is-spaced has-text-white">
+              Blockchain-as-a-Service for Enterprise!
+            </h1>
+            <h2 class="subtitle is-5 has-text-white">
+              Callisto is an enterprise-oriented project with Defi, Cold Staking, and Security features.
+            </h2>
+
+            <h2 class="subtitle is-5 has-text-white">
+              Whatever your size and sector of activity, you can now outsource the entire blockchain technical process and focus on your core expertise.
+            </h2>
+
+            <div class="buttons">
+              <a class="ls-button button has-text-weight-bold is-fullwidth">Get Started</a>
+            </div>
+
+            <div class="news-banner has-text-white">
+              <span class="tag is-size-6-mobile is-size-7">News</span>
+              <span class="is-size-6-mobile is-size-7">Discover how Callisto and Callisto Enterprise is about to change the daily life of millions of users.</span>
+            </div>
+          </div>
+
+          <div class="column right is-5-desktop is-offset-1-desktop">
+            <img class="ls-main-hero--image" src="~/assets/hero-image.svg" width="100%" alt="Layout">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <nuxt />
+
+    <footer class="footer">
+      <div class="container">
+        <div class="columns has-text-centered-mobile">
+          <div class="column is-4">
+            <img
+              class="ls-space"
+              src="https://explorer.callisto.network/images/callisto_logo.svg?v=1"
+              title="Callsito Logo"
+              width="197"
+              height="45"
+            >
+            <p class="has-text-grey" style="padding: 1em">
+              Callisto is an enterprise-oriented project with Defi, Cold Staking, and Security features.
+            </p>
+          </div>
+          <div class="column is-2">
+            <h5 class="title has-text-dark is-5">
+              Cryptocurrency
+            </h5>
+            <ul>
+              <li><a href="/">Wallets</a></li>
+              <li><a href="/">Mining</a></li>
+              <li><a href="/">Exchanges</a></li>
+              <li><a href="/">Partners</a></li>
+              <li><a href="/">Community</a></li>
+            </ul>
+          </div>
+          <div class="column is-2">
+            <h5 class="title has-text-dark is-5">
+              Resources
+            </h5>
+            <ul>
+              <li><a href="/">Security Audits</a></li>
+              <li><a href="/">Deploy Smart Contracts</a></li>
+              <li><a href="/">Migrate Smart Contracts</a></li>
+              <li><a href="/">Tools</a></li>
+              <li><a href="/">Stats</a></li>
+              <li><a href="/">Explorer</a></li>
+            </ul>
+          </div>
+          <div class="column is-2">
+            <h5 class="title has-text-dark is-5">
+              Project
+            </h5>
+            <ul>
+              <li><a href="/">About Us</a></li>
+              <li><a href="/">Support</a></li>
+              <li><a href="/">Contact</a></li>
+              <li><a href="/">Terms &amp; Conditions</a></li>
+              <li><a href="/">FAQ</a></li>
+              <li><a href="/">Press</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="columns ls-footer">
+          <div class="column is-12">
+            <ul class="columns ls-bottom-links is-mobile is-multiline is-marginless">
+              <li class="ls-bottom-link">
+                <a href="/">Privacy Policy</a>
+              </li>
+              <li class="ls-bottom-link">
+                <a href="/">Telegram</a>
+              </li>
+              <li class="ls-bottom-link">
+                <a href="/">Twitter</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.hero {
+  align-items: stretch;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.is-main-hero {
+  padding-top: 1rem;
+  position: relative;
+  overflow: hidden;
+}
+
+.is-main-hero:before {
+  position: absolute;
+  transform: skewY(-5deg);
+  bottom: 7vw;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  content: '';
+  background: linear-gradient(to right, #34c88a 20%, #00CA55) #5FB784;
+}
+
+.news-banner {
+  background: rgba(0,0,0,0.1);
+  padding: .3125rem;
+  border-radius: 30px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+}
+
+.news-banner .tag {
+  border-radius: 20px;
+  padding: .3125rem .625rem;
+  font-size: .75rem;
+  text-transform: uppercase;
+  margin-right: .625rem;
+  display: inline-block;
+  line-height: initial;
+}
+
+.ls-footer .ls-bottom-links {
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.ls-footer .ls-bottom-link {
+  padding: 0 1rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,319 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="columns is-multiline">
+        <div class="column is-12 has-text-centered">
+          <h2 class="is-size-3 has-text-weight-semibold">
+            Featured on
+          </h2>
+        </div>
+
+        <div class="column featured-column">
+          <b-image
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/334244/CNBC-Logo.png"
+            alt="CNBC"
+            width="120px"
+            class="featured-img"
+          />
+        </div>
+
+        <div class="column featured-column">
+          <b-image
+            src="https://upload.wikimedia.org/wikipedia/en/d/da/Fastmoneysept16.jpg"
+            alt="Fast Money"
+            width="120px"
+            class="featured-img"
+          />
+        </div>
+
+        <div class="column featured-column">
+          <b-image
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b8/Cointelegraph_logo.png"
+            alt="Cointelegraph"
+            width="120px"
+            class="featured-img"
+          />
+        </div>
+
+        <div class="column featured-column">
+          <b-image
+            src="https://btctip.cz/wp-content/uploads/2014/04/btctip_logo_claim.jpg"
+            alt="btctip"
+            width="120px"
+            class="featured-img"
+          />
+        </div>
+
+        <div class="column featured-column">
+          <b-image
+            src="https://blockonomi-9fcd.kxcdn.com/wp-content/uploads/2020/01/blockonomi-logo-news.png"
+            alt="blockonomi"
+            width="120px"
+            class="featured-img"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div style="margin-top: 40px; background: #34c88a; color: #fff">
+      <div class="container" style="padding-top: 50px; padding-bottom: 50px">
+        <div class="columns is-multiline">
+          <div class="column is-12 has-text-centered is-full-width">
+            <h2 class="is-size-3 has-text-weight-semibold">
+              Callisto Stats
+            </h2>
+          </div>
+
+          <div class="column is-12">
+            <nav class="level">
+              <div class="level-item has-text-centered">
+                <div>
+                  <p class="heading">
+                    Cold Staking Funds
+                  </p>
+                  <p class="title has-text-white">
+                    +$2.5 million
+                  </p>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <p class="heading">
+                    Unique Wallets
+                  </p>
+                  <p class="title has-text-white">
+                    +59 million
+                  </p>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <p class="heading">
+                    Transactions
+                  </p>
+                  <p class="title has-text-white">
+                    +26 million
+                  </p>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <p class="heading">
+                    Security Audits
+                  </p>
+                  <p class="title has-text-white">
+                    +300
+                  </p>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <p class="heading">
+                    Smart Contracts Hacked
+                  </p>
+                  <p class="title has-text-white">
+                    0
+                  </p>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div style="margin-top: 40px;">
+      <div class="container" style="padding-top: 50px; padding-bottom: 50px">
+        <div class="columns">
+          <div class="column left is-5-desktop">
+            <img src="~/assets/home-puzzle.svg" width="100%" alt="Layout">
+          </div>
+
+          <div class="column right is-5-desktop is-offset-1-desktop content">
+            <h2 class="is-size-3 has-text-weight-semibold">
+              About Callisto Network
+            </h2>
+
+            <p>Callisto Network was created as a community project, but in time it evolved to a professional/enterprise project with team and funding.</p>
+
+            <p>Based on Ethereum blockchain, we created some unique features and do NOT plan to move to POS so we are Miners friendly blockchain.</p>
+
+            <p>For Developers and Partners we have special funds from our partner Callisto Enterprise.</p>
+
+            <p>
+              Blockchain was created by a team that was not happy with Ethereum Classic directions and was responsible for
+              many great features and developments behind ETC.
+            </p>
+
+            <p>Main areas of our focus are: Security, Enterprise applications of blockchain, Defi and Cold stacking.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div style="margin-top: 40px; background: #34c88a">
+      <div class="container" style="padding-top: 50px; padding-bottom: 50px">
+        <div class="columns is-multiline">
+          <div class="column is-12 has-text-centered is-full-width">
+            <h2 class="is-size-3 has-text-weight-semibold has-text-white">
+              Features
+            </h2>
+          </div>
+
+          <div class="column is-one-third">
+            <article class="card ls-feature-card">
+              <div class="card-content">
+                <figure class="image is-159x159">
+                  <img src="img/test.png" title="LandSpeed UI" width="145" height="159">
+                </figure>
+                <div class="media-content has-text-centered">
+                  <h3 class="title is-5 has-text-weight-bold">
+                    <a href="/" class="has-text-dark" title="">Enterprise-grade version</a>
+                  </h3>
+                  <p class="is-size-6">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+
+          <div class="column is-one-third">
+            <article class="card ls-feature-card">
+              <div class="card-content">
+                <figure class="image is-159x159">
+                  <img src="img/test.png" title="LandSpeed UI" width="145" height="159">
+                </figure>
+                <div class="media-content has-text-centered">
+                  <h3 class="title is-5 has-text-weight-bold">
+                    <a href="/" class="has-text-dark" title="">EVM Compatible blockchain</a>
+                  </h3>
+                  <p class="is-size-6">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+
+          <div class="column is-one-third">
+            <article class="card ls-feature-card">
+              <div class="card-content">
+                <figure class="image is-159x159">
+                  <img src="img/test.png" title="LandSpeed UI" width="145" height="159">
+                </figure>
+                <div class="media-content has-text-centered">
+                  <h3 class="title is-5 has-text-weight-bold">
+                    <a href="/" class="has-text-dark" title="">Ethash POW algo</a>
+                  </h3>
+                  <p class="is-size-6">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div style="margin-top: 40px;">
+      <div class="container" style="padding-top: 50px; padding-bottom: 50px">
+        <div class="columns">
+          <div class="column left is-5-desktop content">
+            <h2 class="is-size-3 has-text-weight-semibold">
+              Main Team
+            </h2>
+
+            <div class="columns is-multiline">
+              <div class="column is-4">
+                <div class="team-member-container">
+                  <div>
+                    <figure class="image is-96x96">
+                      <img class="is-rounded" src="https://pbs.twimg.com/profile_images/1309563288850763777/kKPB7j76_400x400.jpg">
+                    </figure>
+                    <!-- Member meta -->
+                    <div class="member-info">
+                      <h4 class="is-light is-6 has-text-centered">Yohan Graterol</h4>
+                      <div class="position has-text-centered">Co-Founder / CEO</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="column is-4">
+                <div class="team-member-container">
+                  <div>
+                    <figure class="image is-96x96">
+                      <img class="is-rounded" src="https://pbs.twimg.com/profile_images/1075015780482338816/z60hTwBa_400x400.jpg">
+                    </figure>
+                    <!-- Member meta -->
+                    <div class="member-info">
+                      <h4 class="is-light is-6 has-text-centered">Dexaran</h4>
+                      <div class="position has-text-centered">Co-Founder / Security Expert</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="column is-4">
+                <div class="team-member-container">
+                  <div>
+                    <figure class="image is-96x96">
+                      <img class="is-rounded" src="https://pbs.twimg.com/profile_images/1024305796539133953/uU1_eoal_400x400.jpg">
+                    </figure>
+                    <!-- Member meta -->
+                    <div class="member-info">
+                      <h4 class="is-light is-6 has-text-centered">Tonton</h4>
+                      <div class="position has-text-centered">CMO</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="column is-4">
+                <div class="team-member-container">
+                  <div>
+                    <figure class="image is-96x96">
+                      <img class="is-rounded" src="https://invictussolutions.cz/wp-content/uploads/2019/04/vencalek-1.jpg">
+                    </figure>
+                    <!-- Member meta -->
+                    <div class="member-info">
+                      <h4 class="is-light is-6 has-text-centered">Vladimír Vencálek</h4>
+                      <div class="position has-text-centered">BizDev / Investor Manager</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="column is-4">
+                <div class="team-member-container">
+                  <div>
+                    <figure class="image is-96x96">
+                      <img class="is-rounded" src="https://pbs.twimg.com/profile_images/1309563288850763777/kKPB7j76_400x400.jpg">
+                    </figure>
+                    <!-- Member meta -->
+                    <div class="member-info">
+                      <h4 class="is-light is-6 has-text-centered">Yohan Graterol</h4>
+                      <div class="position has-text-centered">Founder / CEO</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="column right is-offset-1-desktop is-5-desktop">
+            <img src="~/assets/home-team.svg" width="100%" alt="Layout">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
