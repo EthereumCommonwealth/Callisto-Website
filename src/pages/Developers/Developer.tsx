565aa4d0daf6cca2e73aa1d58160fdf6c52fa1dc
mobiledev-111-118 <mobiledev111118@gmail.com>
2022-01-26 09:52:09 -0800

    added for developer page

--- src/pages/Developers/Developer.tsx
+++ src/pages/Developers/Developer.tsx
@@ -10,9 +10,9 @@ const Developer = () => {
     <Container>
       <Header />
       <Content>
-        {/* <Title>🌐Callisto Network Testnet Faucet is Live!</Title>
-        <Spacer height="30px" /> */}
-        <Text>If you are a developer and want to test your project implementation on Callisto Network, you can now get testnet $TCLO coins with our faucet!</Text>
+        <Title>🌐Callisto Network Testnet Faucet is Live!</Title>
+        <Spacer height="30px" />
+        <Text>If you are a developer and want to test your project implementation on Callisto Network, you can now get testnet CLO ($TCLO) coins with our faucet!</Text>
         <Spacer height="10px" />
         <Row>
           <Text>➡️ Testnet Faucet:</Text>
